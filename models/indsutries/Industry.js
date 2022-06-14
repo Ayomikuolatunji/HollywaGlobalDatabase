8fe713b7ceab4d45b232a0e96e314401181abb39
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-06-14 02:48:21 +0100

    working with fetch all user api and sequelize query

---
+++ models/indsutries/Industry.js
@@ -0,0 +1,25 @@
+
+
+
+module.exports.industries=(sequelize,DataTypes)=>{
+    return sequelize.define('industries',{
+        id:{
+            type:DataTypes.INTEGER,
+            primaryKey:true,
+            autoIncrement:true
+        },
+        name:{
+            type:DataTypes.STRING,
+            allowNull:false
+        },
+        createdAt:{
+            type:DataTypes.DATE,
+            allowNull:false
+        },
+        updatedAt:{
+            type:DataTypes.DATE,
+            allowNull:false
+        }
+    })
+    
+}
