22ad3f8c534742acd7facd0975e7db66422c1080
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-06-14 14:04:57 +0100

    aasociation

--- models/products/products.model.js
+++ models/products/products.model.js
@@ -3,7 +3,42 @@
 const productModel=(sequelize,DataTypes)=>{
 
     return sequelize.define("products",{
-         
+         id:{
+                type:DataTypes.INTEGER,
+                primaryKey:true,
+                autoIncrement:true
+         },
+        name:{
+            type:DataTypes.STRING,
+            allowNull:false,
+            unique:true
+        },
+        price:{
+            type:DataTypes.INTEGER,
+            allowNull:false
+        },
+        description:{
+            type:DataTypes.STRING,
+            allowNull:false
+        },
+        image:{
+            type:DataTypes.STRING,
+            allowNull:false
+        },
+        industryId:{
+            type:DataTypes.INTEGER,
+            allowNull:false
+        },
+        createdAt:{
+            type:DataTypes.DATE,
+            allowNull:false,
+            defaultValue:DataTypes.NOW
+        },
+        updatedAt:{
+            type:DataTypes.DATE,
+            allowNull:false,
+            defaultValue:DataTypes.NOW
+        }
     })
 }
 
