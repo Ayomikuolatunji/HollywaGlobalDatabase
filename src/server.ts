f31140dfd8d7d17f4e47962dd3fd69f55e9423c7
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-07-04 22:05:10 +0100

    clear

--- src/server.ts
+++ src/server.ts
@@ -59,7 +59,7 @@ app.listen(8080, () => {
     sequelize.authenticate().then(()=>{
         console.log('Connection has been established successfully.');
     })
-    .catch((err: { message: any; })=>{
+    .catch((err: { message: string; })=>{
         console.error('Unable to connect to the database:',err.message);
     })
     console.log("Server is running on port 8080");
