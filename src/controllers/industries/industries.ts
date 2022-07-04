dd4dc1ba294e5c10dc0febfd5ee491ff8ae700e7
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-07-04 19:13:44 +0100

    typescript conversion

--- src/controllers/industries/industries.ts
+++ src/controllers/industries/industries.ts
@@ -1,4 +1,4 @@
-const { db } = require("../../models")
+const { db } = require("../../src/models")
 const data =require('../../data.json')
 
 
--- src/controllers/users/userAddress.ts
+++ src/controllers/users/userAddress.ts
@@ -1,4 +1,4 @@
-const { db } = require("../../models")
+const { db } = require("../../src/models")
 
 
 const createAdress=async(req,res,next)=>{
