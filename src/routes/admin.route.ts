9a1ba8f4c4979cb2eb02ea9377c39513419a7dbf
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-08-16 18:13:49 +0100

    protected single admin endpoint

--- src/routes/admin.route.ts
+++ src/routes/admin.route.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import authToken from "../middleware/authToken";
 import { createAdmin, oneAdmin, signInAdmin } from '../controllers/admin/admin';
 
 
@@ -10,7 +11,7 @@ const router = express.Router();
 // admin routes
 router.post("/create_admin", createAdmin)
 router.post("/admin_signin", signInAdmin)
-router.get("/admin/:adminId", oneAdmin)
+router.get("/admin/:adminId",authToken,oneAdmin)
 
 
 
