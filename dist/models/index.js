bf400045873458ad09284b2472463ad57fae16f0
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-09-13 20:36:43 +0100

    build start bug fix

--- dist/models/index.js
+++ dist/models/index.js
@@ -22,17 +22,15 @@ const industry_model_1 = __importDefault(require("./industries/industry.model"))
 const userAddress_model_1 = __importDefault(require("./user/userAddress.model"));
 const product_category_model_1 = __importDefault(require("./products/product_category.model"));
 const admin_model_1 = __importDefault(require("./admin/admin.model"));
-
 const sequelize = new sequelize_1.Sequelize(dbConfig_1.default.DB, dbConfig_1.default.USER, dbConfig_1.default.PASSWORD, {
-    operatorsAliases: false,
     host: dbConfig_1.default.HOST,
     dialect: "mysql",
     pool: {
         max: dbConfig_1.default.pool.max,
         min: dbConfig_1.default.pool.min,
         acquire: dbConfig_1.default.pool.acquire,
-        idle: dbConfig_1.default.pool.idle,
-    },
+        idle: dbConfig_1.default.pool.idle
+    }
 });
 exports.sequelize = sequelize;
 const db = {
@@ -43,7 +41,7 @@ const db = {
     industries: (0, industry_model_1.default)(sequelize, sequelize_1.DataTypes),
     userAddressModel: (0, userAddress_model_1.default)(sequelize, sequelize_1.DataTypes),
     product_category: (0, product_category_model_1.default)(sequelize, sequelize_1.DataTypes),
-    admin: (0, admin_model_1.default)(sequelize, sequelize_1.DataTypes),
+    admin: (0, admin_model_1.default)(sequelize, sequelize_1.DataTypes)
 };
 exports.db = db;
 /*=============================================
@@ -51,23 +49,23 @@ exports.db = db;
 =============================================*/
 //products  association  here
 db.products.belongsTo(db.admin, {
-    foreignKey: "adminId",
-    onDelete: "CASCADE",
+    foreignKey: 'adminId',
+    onDelete: 'CASCADE',
 });
 db.admin.hasMany(db.products, {
-    onDelete: "CASCADE",
+    onDelete: "CASCADE"
 });
 db.products.hasMany(db.product_category, {
-    foreignKey: "categoryId",
+    foreignKey: 'categoryId',
 });
 // user association here
 db.user.hasOne(db.userAddressModel, {
-    foreignKey: "userId",
-    onDelete: "CASCADE",
+    foreignKey: 'userId',
+    onDelete: 'CASCADE'
 });
 db.userPaymentModel.belongsTo(db.user, {
-    foreignKey: "userId",
-    onDelete: "CASCADE",
+    foreignKey: 'userId',
+    onDelete: 'CASCADE'
 });
 /*=====  End of All table associations======*/
 /*=============================================
@@ -76,10 +74,10 @@ db.userPaymentModel.belongsTo(db.user, {
 const DB = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
         yield db.sequelize.sync({ force: false });
-        console.log("Tables created successfully.");
+        console.log('Tables created successfully.');
     }
     catch (err) {
-        console.error("Unable to create tables:", err.message);
+        console.error('Unable to create tables:', err.message);
     }
 });
 DB();
