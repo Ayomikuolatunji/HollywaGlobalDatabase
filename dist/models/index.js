92abf8970b351cad8c059fc96362433d201af13b
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-09-13 20:33:04 +0100

    build start bug fix

--- dist/models/index.js
+++ dist/models/index.js
@@ -22,44 +22,9 @@ const industry_model_1 = __importDefault(require("./industries/industry.model"))
 const userAddress_model_1 = __importDefault(require("./user/userAddress.model"));
 const product_category_model_1 = __importDefault(require("./products/product_category.model"));
 const admin_model_1 = __importDefault(require("./admin/admin.model"));
-const operatorsAliases = {
-    $eq: sequelize_1.Op.eq,
-    $ne: sequelize_1.Op.ne,
-    $gte: sequelize_1.Op.gte,
-    $gt: sequelize_1.Op.gt,
-    $lte: sequelize_1.Op.lte,
-    $lt: sequelize_1.Op.lt,
-    $not: sequelize_1.Op.not,
-    $in: sequelize_1.Op.in,
-    $notIn: sequelize_1.Op.notIn,
-    $is: sequelize_1.Op.is,
-    $like: sequelize_1.Op.like,
-    $notLike: sequelize_1.Op.notLike,
-    $iLike: sequelize_1.Op.iLike,
-    $notILike: sequelize_1.Op.notILike,
-    $regexp: sequelize_1.Op.regexp,
-    $notRegexp: sequelize_1.Op.notRegexp,
-    $iRegexp: sequelize_1.Op.iRegexp,
-    $notIRegexp: sequelize_1.Op.notIRegexp,
-    $between: sequelize_1.Op.between,
-    $notBetween: sequelize_1.Op.notBetween,
-    $overlap: sequelize_1.Op.overlap,
-    $contains: sequelize_1.Op.contains,
-    $contained: sequelize_1.Op.contained,
-    $adjacent: sequelize_1.Op.adjacent,
-    $strictLeft: sequelize_1.Op.strictLeft,
-    $strictRight: sequelize_1.Op.strictRight,
-    $noExtendRight: sequelize_1.Op.noExtendRight,
-    $noExtendLeft: sequelize_1.Op.noExtendLeft,
-    $and: sequelize_1.Op.and,
-    $or: sequelize_1.Op.or,
-    $any: sequelize_1.Op.any,
-    $all: sequelize_1.Op.all,
-    $values: sequelize_1.Op.values,
-    $col: sequelize_1.Op.col,
-};
+
 const sequelize = new sequelize_1.Sequelize(dbConfig_1.default.DB, dbConfig_1.default.USER, dbConfig_1.default.PASSWORD, {
-    operatorsAliases: operatorsAliases,
+    operatorsAliases: false,
     host: dbConfig_1.default.HOST,
     dialect: "mysql",
     pool: {
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,100 +1,69 @@
-import { Sequelize, DataTypes, Op } from "sequelize";
+import { Sequelize, DataTypes } from 'sequelize';
 
-import config from "../database/dbConfig";
-import userModel from "./user/user.model";
-import productModel from "./products/products.model";
-import userPaymentModel from "./user/payment.model";
-import industryModel from "./industries/industry.model";
-import userAddressModel from "./user/userAddress.model";
-import produtCategory from "./products/product_category.model";
-import adminModel from "./admin/admin.model";
+import config from '../database/dbConfig';
+import userModel from './user/user.model';
+import productModel from './products/products.model';
+import userPaymentModel from './user/payment.model';
+import industryModel from './industries/industry.model';
+import userAddressModel from './user/userAddress.model';
+import produtCategory from './products/product_category.model';
+import adminModel from './admin/admin.model';
 
-const operatorsAliases = {
-    $eq: Op.eq,
-    $ne: Op.ne,
-    $gte: Op.gte,
-    $gt: Op.gt,
-    $lte: Op.lte,
-    $lt: Op.lt,
-    $not: Op.not,
-    $in: Op.in,
-    $notIn: Op.notIn,
-    $is: Op.is,
-    $like: Op.like,
-    $notLike: Op.notLike,
-    $iLike: Op.iLike,
-    $notILike: Op.notILike,
-    $regexp: Op.regexp,
-    $notRegexp: Op.notRegexp,
-    $iRegexp: Op.iRegexp,
-    $notIRegexp: Op.notIRegexp,
-    $between: Op.between,
-    $notBetween: Op.notBetween,
-    $overlap: Op.overlap,
-    $contains: Op.contains,
-    $contained: Op.contained,
-    $adjacent: Op.adjacent,
-    $strictLeft: Op.strictLeft,
-    $strictRight: Op.strictRight,
-    $noExtendRight: Op.noExtendRight,
-    $noExtendLeft: Op.noExtendLeft,
-    $and: Op.and,
-    $or: Op.or,
-    $any: Op.any,
-    $all: Op.all,
-    $values: Op.values,
-    $col: Op.col,
-};
 
-const sequelize = new Sequelize(config.DB!, config.USER!, config.PASSWORD!, {
-  operatorsAliases:operatorsAliases,
-  host: config.HOST!,
-  dialect: "mysql",
-  pool: {
-    max: config.pool.max,
-    min: config.pool.min,
-    acquire: config.pool.acquire,
-    idle: config.pool.idle,
-  },
-});
+const sequelize = new Sequelize(
+    config.DB!,
+    config.USER!,
+    config.PASSWORD!, {
+    host: config.HOST!,
+    dialect: "mysql",
+    pool: {
+        max: config.pool.max,
+        min: config.pool.min,
+        acquire: config.pool.acquire,
+        idle: config.pool.idle
+    }
+}
+)
 
 const db = {
-  sequelize,
-  user: userModel(sequelize, DataTypes),
-  products: productModel(sequelize, DataTypes),
-  userPaymentModel: userPaymentModel(sequelize, DataTypes),
-  industries: industryModel(sequelize, DataTypes),
-  userAddressModel: userAddressModel(sequelize, DataTypes),
-  product_category: produtCategory(sequelize, DataTypes),
-  admin: adminModel(sequelize, DataTypes),
-};
+    sequelize,
+    user: userModel(sequelize, DataTypes),
+    products: productModel(sequelize, DataTypes),
+    userPaymentModel: userPaymentModel(sequelize, DataTypes),
+    industries: industryModel(sequelize, DataTypes),
+    userAddressModel: userAddressModel(sequelize, DataTypes),
+    product_category: produtCategory(sequelize, DataTypes),
+    admin: adminModel(sequelize, DataTypes)
+}
+
 
 /*=============================================
 =            All table associations          =
 =============================================*/
 
+
 //products  association  here
 db.products.belongsTo(db.admin, {
-  foreignKey: "adminId",
-  onDelete: "CASCADE",
-});
+    foreignKey: 'adminId',
+    onDelete: 'CASCADE',
+})
 db.admin.hasMany(db.products, {
-  onDelete: "CASCADE",
-});
+    onDelete: "CASCADE"
+})
 
 db.products.hasMany(db.product_category, {
-  foreignKey: "categoryId",
-});
+    foreignKey: 'categoryId',
+})
 
 // user association here
 db.user.hasOne(db.userAddressModel, {
-  foreignKey: "userId",
-  onDelete: "CASCADE",
-});
+    foreignKey: 'userId',
+    onDelete: 'CASCADE'
+})
 db.userPaymentModel.belongsTo(db.user, {
-  foreignKey: "userId",
-  onDelete: "CASCADE",
-});
+    foreignKey: 'userId',
+    onDelete: 'CASCADE'
+})
 
 /*=====  End of All table associations======*/
 
@@ -103,18 +72,21 @@ db.userPaymentModel.belongsTo(db.user, {
 =============================================*/
 
 const DB = async () => {
-  try {
-    await db.sequelize.sync({ force: false });
-    console.log("Tables created successfully.");
-  } catch (err: any) {
-    console.error("Unable to create tables:", err.message);
-  }
-};
+    try {
+        await db.sequelize.sync({ force: false })
+        console.log('Tables created successfully.');
+    } catch (err: any) {
+        console.error('Unable to create tables:', err.message);
+    }
+}
 
-DB();
+DB()
 
 /*=====  End of  create tables ======*/
 
 // send to the controllers and server
 
-export { db, sequelize };
+export {
+    db,
+    sequelize,
+}
