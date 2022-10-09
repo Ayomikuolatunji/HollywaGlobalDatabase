7266f99a8501099b6a3e92bfaa9599948a678dc1
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-10-09 14:13:36 +0100

    user cart api endpoints

--- src/routes/cart.route.ts
+++ src/routes/cart.route.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import { createProductsCart } from "../controllers/cart/ProductCart";
+import authToken from "../middleware/authToken";
 
 const router = express.Router();
 
-router.post("/add_product_to_cart", createProductsCart);
+router.post("/add_product_to_cart", authToken, createProductsCart);
 
 export default router;
--- src/services/v1Api.ts
+++ src/services/v1Api.ts
@@ -2,7 +2,7 @@ const api=require('express').Router();
 import userRouter from "../routes/user.route";
 import productRouter from "../routes/products.route";
 import adminRouter from "../routes/admin.route";
-
+import cartRouter from "../routes/cart.route"
 // user routes
 api.use("/v1", userRouter)
 
